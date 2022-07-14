18f6d947ccfc7756f2500b7b77cc3bdfabb03cc0
PollYrad <102154770+pollyard@users.noreply.github.com>
2022-07-14 13:32:22 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <h1>.</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+        <div>
+            <h3>이곳은 헤더입니다.</h3>
+        </div>
+    )
+}
